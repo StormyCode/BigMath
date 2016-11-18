4e19d718628baf6080a0582197876bf10e96fbb6
J0th4 <julian.heep@gmail.com>
2016-11-18 17:37:57 +0100

    issue #1

--- BigMath/BigMath/Class1.cs
+++ BigMath/BigMath/Class1.cs
@@ -5,8 +5,20 @@ using System.Text;
 
 namespace BigMath
 {
-    public class Class1
+    public class BigMath
     {
-
+        public static int GreatestCommonDivisor(int int1, int int2)
+        {
+            int min = Math.Min(int1, int2);
+            int max = Math.Max(int1, int2);
+            for (int i = min - 1; i >= 2; i--)
+            {
+                if (min % i == 0 && max % i == 0)
+                {
+                    return i;
+                }
+            }
+            return 1;
+        }
     }
 }
